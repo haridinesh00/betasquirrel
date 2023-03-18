d186e8ad8875b666e5c292c09a96142151e90d6f
haridinesh00 <haridinesh00@gmail.com>
2023-03-18 19:45:52 +0530

    removed eval in task8

--- task-8/js/script.js
+++ task-8/js/script.js
@@ -1,13 +1,91 @@
-function CheckandPrint(){
-    try {
-        var x = document.getElementById("inp_field").value;
-        var y = document.getElementById("inp_field");
+// function CheckandPrint(){
+//     try {
+//         var x = document.getElementById("inp_field");
         
-        let result = eval(x);
-        // console.log(result);
-        y.value = result;
+//         let result = eval(x.value);
+//         // console.log(result);
+//         x.value = result;
+//     }
+//     catch(err){
+//         x.value = "ERROR: Enter valid inputs";
+//     }
+// }
+
+var answer = 0;
+var last_opr = '';
+var temp;
+function getElementValue(){
+    var x = document.getElementById("inp_field");
+    return x;
+}
+function clear_screen(){
+    temp = getElementValue();
+    temp.value = '';
+    last_opr = '';
+    answer = 0;
+}
+function lastOp(y){
+    if(last_opr == '+'){
+        answer = answer + Number(y.value);
     }
-    catch(err){
-        y.value = "ERROR: Enter valid inputs";
+    else if(last_opr == '-'){
+        answer = answer - Number(y.value);
     }
+    else if(last_opr == '*'){
+        answer = answer * Number(y.value);
+    }
+    else if(last_opr == '/'){
+        answer = answer / Number(y.value);
+    }
+    return answer;
+}
+function plus(){
+    temp = getElementValue();
+    answer = Number(temp.value);
+    var z = lastOp(temp);
+    last_opr = '+';
+    temp.value = '';
+}
+function minus(){
+    temp = getElementValue();
+    if(temp.value == ''){
+        temp.value += '-';
+    }
+    else {
+        answer = Number(temp.value);
+        var z = lastOp(temp);
+        last_opr = '-';
+        temp.value = '';
+    }
+}
+function divide(){
+    temp = getElementValue();
+    if(temp.value == ''){
+        last_opr = '';
+    }
+    else {
+        answer = Number(temp.value);
+        var z = lastOp(temp);
+        last_opr = '/';
+        temp.value = '';
+    }
+}
+function multiply(){
+    temp = getElementValue();
+    if(temp.value == ''){
+        last_opr = '';
+    }
+    else {
+        answer = Number(temp.value);
+        var z = lastOp(temp);
+        last_opr = '*';
+        temp.value = '';
+    }
+}
+function solution(){
+    temp = getElementValue();
+    var z = lastOp(temp);
+    temp.value = z;
+    answer = 0;
+    last_opr = '';
 }
